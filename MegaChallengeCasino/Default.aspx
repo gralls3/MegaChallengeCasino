e2ca92f6bc657bc8fb6fced67eb0c61413a18629
George Ralls <rallsiii@yahoo.com>
2018-01-20 13:44:34 -0800

    Created random image generator
    Used Methods per instructions.

---
+++ MegaChallengeCasino/Default.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="MegaChallengeCasino.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image ID="slotImage1" runat="server" Height="174px" Width="143px" />
+            <asp:Image ID="slotImage2" runat="server" Height="174px" Width="143px" />
+            <asp:Image ID="slotImage3" runat="server" Height="174px" Width="143px" />
+            <br />
+            <br />
+            Your bet: <asp:TextBox ID="betTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="leverButton" runat="server" Text="Pull the Lever!" OnClick="leverButton_Click" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            Player&#39;s Money:
+            <asp:Label ID="moneyLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            1 Cherry = x2 Your Bet<br />
+            2 Cherries - x3 Your Bet<br />
+            3 Cherries - x4 Your Bet<br />
+            4 7s - Jackpot - x100 Your Bet<br />
+            HOWERVER ... If there&#39;s even one bar, you win nothing.</div>
+    </form>
+</body>
+</html>
---
+++ MegaChallengeCasino/Default.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MegaChallengeCasino
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                slotImage1.ImageUrl = "Images/Bar.png";
+                slotImage2.ImageUrl = "Images/Bar.png";
+                slotImage3.ImageUrl = "Images/Bar.png";
+            }
+        }
+
+        String[] imageURLs ={
+            "Images/Bell.png", "Images/Clover.png", "Images/Diamond.png",
+            "Images/HorseShoe.png", "Images/Lemon.png", "Images/Orange.png",
+            "Images/Plum.png", "Images/Strawberry.png", "Images/Watermellon.png"};
+        Random rnd = new Random();
+
+        protected void leverButton_Click(object sender, EventArgs e)
+        {
+            generateImages();
+        }
+
+        private void generateImages()
+        {
+            generateImage1();
+            generateImage2();
+            generateImage3();
+        }
+
+        private void generateImage1()
+        {
+            int index = rnd.Next(0, imageURLs.Length);
+            slotImage1.ImageUrl = imageURLs[index];
+        }
+
+        private void generateImage2()
+        {
+            int index = rnd.Next(0, imageURLs.Length);
+            slotImage2.ImageUrl = imageURLs[index];
+        }
+
+        private void generateImage3()
+        {
+            int index = rnd.Next(0, imageURLs.Length);
+            slotImage3.ImageUrl = imageURLs[index];
+        }
+    }
+}
